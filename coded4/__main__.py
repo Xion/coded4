81d0e826e6b5619e92642224837ce9b12ad12e98
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2011-12-24 22:57:18 +0100

    Adding '--algo' command line option

--- coded4/__main__.py
+++ coded4/__main__.py
@@ -18,6 +18,7 @@ def main():
 
     if args:
         vcs_name = args.vcs or detect_vcs(args.directory)
+
         contributors = calculate_stats(args.directory, vcs_name, args.initial_time, args.break_time)
         contributors = sorted(contributors, key = lambda c: len(c.commits), reverse=True)
 
@@ -29,26 +30,35 @@ def create_argument_parser():
     parser = argparse.ArgumentParser(description="Calculate time spent coding by using commit timestamps",
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     
-    minutes = lambda m: timedelta(minutes=int(m))
-
+    # add general arguments
     parser.add_argument('directory', type=str, default='.',
                         help="Directory where the repository is contained     (. by default)",
                         metavar="DIRECTORY")
     parser.add_argument('--repo', '-r', type=str, default=None, choices=SUPPORTED_VCS,
                         help="Repository type for which the stats should be generated",
                         metavar="TYPE", dest='vcs')
+    parser.add_argument('--algorithm', '--algo', '-a', default='simple', choices=CLUSTERING_ALGORITHMS,
+                        help="What algorithm should be used to approximate coding sessions",
+                        metavar="ALGO", dest='algorithm')
+    parser.add_argument('--format', '-f', type=str, default='table', choices=OUTPUT_FORMATS,
+                        help="Output format (formatted table by default)",
+                        metavar="FORMAT", dest='output')
+
+    minutes = lambda m: timedelta(minutes=int(m))
+
+    # add arguments for clustering algorithms
     parser.add_argument('--break', '-b', type=minutes, default=DEFAULT_BREAK_TIME,
                         help="Maximum time between commits which are still considered a single coding session",
                         metavar="MINUTES", dest='break_time')
     parser.add_argument('--initial', '-i', type=minutes, default=DEFAULT_INITIAL_TIME,
                         help="Time before first commit within a coding session",
                         metavar="MINUTES", dest='initial_time')
-    parser.add_argument('--format', '-f', type=str, default='table', choices=['table', 'json'],
-                        help="Output format (formatted table by default)",
-                        metavar="FORMAT", dest='output')
 
     return parser
 
+
+CLUSTERING_ALGORITHMS = ['simple', 'dbscan']
+OUTPUT_FORMATS = ['table', 'json']
 
 DEFAULT_BREAK_TIME = timedelta(minutes=30)
 DEFAULT_INITIAL_TIME = timedelta(minutes=10)
