4c9155302bba74065b3cb9a030ca8a295901eae1
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-08-03 14:13:22 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,67 @@
 #!/usr/bin/env python
-'''
+"""
 coded4
 Setup script
-'''
+"""
+import ast
+import os
 from setuptools import setup, find_packages
-import coded4
+
+
+def read_tags(filename):
+    """Reads values of "magic tags" defined in the given Python file.
+
+    :param filename: Python filename to read the tags from
+    :return: Dictionary of tags
+    """
+    with open(filename) as f:
+        ast_tree = ast.parse(f.read(), filename)
+
+    res = {}
+    for node in ast.walk(ast_tree):
+        if type(node) is not ast.Assign:
+            continue
+
+        target = node.targets[0]
+        if type(target) is not ast.Name:
+            continue
+
+        if not (target.id.startswith('__') and target.id.endswith('__')):
+            continue
+
+        name = target.id[2:-2]
+        res[name] = ast.literal_eval(node.value)
+
+    return res
+
+
+tags = read_tags(os.path.join('coded4', '__init__.py'))
 
 
 setup(name="coded4",
-	  version=coded4.__version__,
-	  description="Time-based statistics for Git and Hg repositories",
-	  author='Karol Kuczmarski "Xion"',
-	  author_email="karol.kuczmarski@gmail.com",
-	  url="http://github.com/Xion/coded4",
-	  license="MIT",
+      version=tags['__version__'],
+      description="Time-based statistics for Git and Hg repositories",
+      author=tags['__author__'],
+      author_email="karol.kuczmarski@gmail.com",
+      url="http://github.com/Xion/coded4",
+      license=tags['__license__'],
 
-	  classifiers = [
-	     'Development Status :: 4 - Beta',
-	     'Intended Audience :: Developers',
-	     'Intended Audience :: Information Technology',
-	     'License :: OSI Approved :: MIT License',
-	     'Operating System :: OS Independent',
-	     'Programming Language :: Python',
+      classifiers = [
+         'Development Status :: 4 - Beta',
+         'Intended Audience :: Developers',
+         'Intended Audience :: Information Technology',
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: OS Independent',
+         'Programming Language :: Python',
          'Programming Language :: Python :: 2.6',
          'Programming Language :: Python :: 2.7',
          'Topic :: Software Development',
-	  ],
+      ],
 
-	  packages = find_packages(),
-	  entry_points = {
-	  	  'console_scripts': [
-	  	      'coded4 = coded4.__main__:main',
-	  	  ],
-	  },
+      packages = find_packages(),
+      entry_points = {
+          'console_scripts': [
+              'coded4 = coded4.__main__:main',
+          ],
+      },
 )
