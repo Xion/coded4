0e19c7d293e742f6f5a29c53b6ecc5591a5f91f9
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2014-08-03 15:04:48 -0700

    Fix cruft in __main__.py

--- coded4/__main__.py
+++ coded4/__main__.py
@@ -4,15 +4,11 @@ Executable script.
 """
 from __future__ import unicode_literals
 
-import vcs
-import cluster
-import approx
-import stats
-from output import format_output
-
-from datetime import datetime, timedelta
 import argparse
+from datetime import datetime, timedelta
 
+from coded4 import approx, cluster, stats, vcs
+from coded4.output import format_output
 
 
 def main():
@@ -25,19 +21,23 @@ def main():
 
 
 def create_argument_parser():
-    parser = argparse.ArgumentParser(description="Calculate time spent coding by using commit timestamps",
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description="Calculate time spent coding by using commit timestamps",
+        formatter_class=argparse.RawDescriptionHelpFormatter)
 
     # add general arguments
-    parser.add_argument('directory', type=str, default='.',
-                        help="Directory where the repository is contained (. by default)",
-                        metavar="DIRECTORY")
-    parser.add_argument('--repo', '-r', type=str, default=None, choices=vcs.SUPPORTED_VCS,
-                        help="Repository type for which the stats should be generated",
-                        metavar="TYPE", dest='vcs')
-    parser.add_argument('--format', '-f', type=str, default='table', choices=OUTPUT_FORMATS,
-                        help="Output format (formatted table by default)",
-                        metavar="FORMAT", dest='output')
+    parser.add_argument(
+        'directory', type=str, default='.',
+        help="Directory where the repository is contained (. by default)",
+        metavar="DIRECTORY")
+    parser.add_argument(
+        '--repo', '-r', type=str, default=None, choices=vcs.SUPPORTED_VCS,
+        help="Repository type for which the stats should be generated",
+        metavar="TYPE", dest='vcs')
+    parser.add_argument(
+        '--format', '-f', type=str, default='table', choices=OUTPUT_FORMATS,
+        help="Output format (formatted table by default)",
+        metavar="FORMAT", dest='output')
 
     # add filtering arguments
     timespec = lambda s: datetime.strptime(s, DATETIME_FORMAT)
@@ -51,21 +51,26 @@ def create_argument_parser():
                         metavar="DATE", dest='until')
 
     # add algorithms
-    parser.add_argument('--cluster-algo', '-c', default='simple', choices=CLUSTERING_ALGORITHMS,
-                        help="What algorithm should be used to cluster individual commits. "
-                        + "Possible values: %(choices)s",
-                        metavar="ALGO", dest='cluster_algo')
-    parser.add_argument('--approx-algo', '-a', default='polynomial', choices=APPROXIMATION_ALGORITHMS,
-                        help="What algorithms should be used to approximate time spent coding. "
-                        + "Possible values: %(choices)s",
-                        metavar="ALGO", dest='approx_algo')
+    parser.add_argument(
+        '--cluster-algo', '-c', default='simple', choices=CLUSTERING_ALGORITHMS,
+        help="What algorithm should be used to cluster individual commits. "
+             "Possible values: %(choices)s",
+        metavar="ALGO", dest='cluster_algo')
+    parser.add_argument(
+        '--approx-algo', '-a',
+        default='polynomial', choices=APPROXIMATION_ALGORITHMS,
+        help="What algorithms should be used to approximate time spent coding. "
+             "Possible values: %(choices)s",
+        metavar="ALGO", dest='approx_algo')
 
     # add other options
     minutes = lambda m: timedelta(minutes=int(m))
-    parser.add_argument('--epsilon', '--eps', '-e', type=minutes, default=minutes(DEFAULT_EPSILON_MINUTES),
-                        help="Maximum time between commits which are still considered a single coding session "
-                        + "(default: %s)" % DEFAULT_EPSILON_MINUTES,
-                        metavar="MINUTES", dest='epsilon')
+    parser.add_argument(
+        '--epsilon', '--eps', '-e', type=minutes,
+        default=minutes(DEFAULT_EPSILON_MINUTES),
+        help="Maximum time between commits which are still considered "
+             "a single coding session (default: %s)" % DEFAULT_EPSILON_MINUTES,
+        metavar="MINUTES", dest='epsilon')
 
     return parser
 
@@ -74,11 +79,15 @@ DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 OUTPUT_FORMATS = ['table', 'json']
 CLUSTERING_ALGORITHMS = ['simple']
 APPROXIMATION_ALGORITHMS = {
-    'null': "Null approximation (i.e. uses only time between commits), used mostly for testing",
+    'null': "Null approximation (i.e. uses only time between commits), "
+            "used mostly for testing",
     'start10': "Simple algorithm that adds 10 minutes before first commit",
-    'ten2five': "Simple algorithm that adds 10 minutes before first and 5 minutes after last commit",
-    'quarter_end': "Uses average time between commits in session, adding 1/4th of it after last commit",
-    'polynomial': "Fits a polynomial into intervals between commits and extrapolates it",
+    'ten2five': "Simple algorithm that adds 10 minutes before first "
+                "and 5 minutes after last commit",
+    'quarter_end': "Uses average time between commits in session, "
+                   "adding 1/4th of it after last commit",
+    'polynomial': "Fits a polynomial into intervals between commits "
+                  "and extrapolates it for time before first and after last",
 }
 
 DEFAULT_EPSILON_MINUTES = 30
@@ -90,10 +99,15 @@ def calculate_statistics(args):
     """Calculates statistics, as dictated by command line args.
     :return: List of Contributor tuples
     """
-    commit_history = vcs.retrieve_commit_history(args.directory, args.vcs, (args.since, args.until))
+    commit_history = vcs.retrieve_commit_history(
+        args.directory, args.vcs, (args.since, args.until))
+
     grouped_commits = cluster.group_by_contributors(commit_history)
-    clustered_commits = cluster.cluster_commits(grouped_commits, args.cluster_algo, args.epsilon)
-    coding_sessions = approx.approximate_coding_sessions(clustered_commits, args.approx_algo)
+    clustered_commits = cluster.cluster_commits(
+        grouped_commits, args.cluster_algo, args.epsilon)
+
+    coding_sessions = approx.approximate_coding_sessions(
+        clustered_commits, args.approx_algo)
     contributors = stats.compute_time_stats(coding_sessions)
 
     return sorted(contributors, key = lambda c: len(c.commits), reverse=True)
